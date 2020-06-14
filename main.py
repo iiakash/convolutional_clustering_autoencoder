2a83af78f3838fa263b12c61900426eaa73e78f5
Intekhab Islam <50600530+iiakash@users.noreply.github.com>
2020-06-14 20:17:18 +0200

    Cleaning

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 import time
 import numpy as np
         
-import anomaly_detection
+import ad_class
 import networks
 import torch
 import torch.nn as nn
-import helpers
-
-#take inputs
+import functions
 
 mode = input('Run Mode: ')
 network_criterion = nn.MSELoss()
@@ -29,11 +27,6 @@ downsampling_step = int(input('Downsampling Step: '))
 sequence_length = int(input('Time Series Sequence Length: '))
 no_of_map = int(input('No of Best Activation Maps for Plotting: '))
 
-#start timer
-
-
-
-#initiate the suitable network
 
 network = networks.CAE_3_bn(input_channels = input_channels, filter_multiplier = filter_multiplier, kernel_size = kernel_size, leaky = True, neg_slope = 0.01, activations = False)
 
@@ -41,31 +34,27 @@ network = networks.CAE_3_bn(input_channels = input_channels, filter_multiplier =
 since = time.time()
 
 if mode == 'Bultmann':
-    #initiate the run class
-    run = anomaly_detection.AnomalyDetection(mode, network, network_criterion, n_clusters, 
+
+    run = ad_class.AnomalyDetection(mode, network, network_criterion, n_clusters, 
                clustering_criterion, cluster_update_interval,
                no_of_clustering_channels, n_epochs, no_of_pretrain_epochs, 
                batch_size, lr, alpha,downsampling_step, sequence_length, kernel_size)
     
-    #load the data
-    
+
     train_loader, train_data_len, test_loader, test_data_len = run.load_bultmann_data(normalize = True)
       
-    #train the network
 
     network, optimizer, list_of_network_loss, list_of_clustering_loss,list_of_total_loss, list_of_losses, embeddings,labels, list_of_centers, list_of_ranks_of_center_distances, list_of_center_distances = run.train_bultmann(train_loader, train_data_len, Adam = False, scheduler = False)
     
-    #training complete
-    
-    #final kmeans on the trained embedded space
+
         
     labels_pred = run.training_predictions(embeddings)
         
-    #compute the training metrics
+
         
     list_of_nmi, list_of_ari, list_of_acc, list_of_cm = run.calculate_metrics(labels, labels_pred)
     
-    #best metrics and top embedding channels
+
         
     best_training_nmi, best_training_ari, best_training_acc, top_nmi_maps_idx, top_ari_maps_idx, top_acc_maps_idx = run.best_activation_maps(list_of_nmi, list_of_ari, list_of_acc, list_of_cm, no_of_map)
     
@@ -73,40 +62,36 @@ if mode == 'Bultmann':
     
     test_labels_pred = run.training_predictions(embeddings)
         
-    #compute the training metrics
+
         
     list_of_nmi_test, list_of_ari_test, list_of_acc_test, list_of_cm_test = run.calculate_metrics(test_labels_true, test_labels_pred)
     
-    #best metrics and top embedding channels
+
         
     best_testing_nmi, best_testing_ari, best_testing_acc, top_nmi_maps_idx_test, top_ari_maps_idx_test, top_acc_maps_idx_test = run.best_activation_maps(list_of_nmi_test, list_of_ari_test, list_of_acc_test, list_of_cm_test, no_of_map)
     
 elif mode == 'TEastman':
-    #initiate the run class
+
     run = anomaly_detection.AnomalyDetection(mode, network, network_criterion, n_clusters, 
                clustering_criterion, cluster_update_interval,
                no_of_clustering_channels, n_epochs, no_of_pretrain_epochs, 
                batch_size, lr, alpha,downsampling_step, sequence_length, kernel_size)
     
-    #load the data 
+
     
     train_loader, test_loader, _ = run.load_teastman_data(normalize = True)
-    
-    #train the network
+
     
     network, optimizer, list_of_network_loss, list_of_clustering_loss,list_of_total_loss, list_of_losses, embeddings, labels, list_of_centers, list_of_ranks_of_center_distances, list_of_center_distances = run.train_teastman(train_loader, 9920, Adam = False, scheduler = True)
-    
-    #training complete
-    
-    #final kmeans on the trained embedded space
+ 
         
     labels_pred = run.training_predictions(embeddings)
     
-    #compute the training metrics
+    
     labels[np.where(labels > 0)] = 1  
     list_of_nmi, list_of_ari, list_of_acc, list_of_cm = run.calculate_metrics(labels[1:], labels_pred)
     
-    #best metrics and top embedding channels
+ 
         
     best_training_nmi, best_training_ari, best_training_acc, top_nmi_maps_idx, top_ari_maps_idx, top_acc_maps_idx = run.best_activation_maps(list_of_nmi, list_of_ari, list_of_acc, list_of_cm, no_of_map)
 
@@ -114,33 +99,27 @@ elif mode == 'TEastman':
     
     test_labels_pred = run.training_predictions(test_embeddings)
         
-    #compute the training metrics
+   
     test_labels_true[np.where(test_labels_true > 0)] = 1   
     list_of_nmi_test, list_of_ari_test, list_of_acc_test, list_of_cm_test = run.calculate_metrics(test_labels_true[1:], test_labels_pred)
     
-    #best metrics and top embedding channels
+   
         
     best_testing_nmi, best_testing_ari, best_testing_acc, top_nmi_maps_idx_test, top_ari_maps_idx_test, top_acc_maps_idx_test = run.best_activation_maps(list_of_nmi_test, list_of_ari_test, list_of_acc_test, list_of_cm_test, no_of_map)
     
 elif mode == 'TEastmanFull':
     
-    #initiate the run class
+    
     run = anomaly_detection.AnomalyDetection(mode, network, network_criterion, n_clusters, 
                clustering_criterion, cluster_update_interval,
                no_of_clustering_channels, n_epochs, no_of_pretrain_epochs, 
                batch_size, lr, alpha,downsampling_step, sequence_length, kernel_size)
     
-    #load the data
     
     data_set, train_loader = run.load_teastman_full_data(train = True, normalize = True)
     
-    #train the network
-    
     network, optimizer, list_of_network_loss, list_of_clustering_loss,list_of_total_loss, list_of_losses, embeddings, labels, list_of_centers,list_of_ranks_of_center_distances, list_of_center_distances = run.train_teastman(train_loader, 9260, Adam = False, scheduler = True)
-
-
-    
-#save 
+ 
 run.save_network(network)
 run.save_optimizer(optimizer)
 run.save_embeddings(embeddings)
@@ -151,7 +130,7 @@ run.save_list_ranks_of_center_distances(list_of_ranks_of_center_distances)
 run.save_train_metrics(list_of_nmi, list_of_ari, list_of_acc, list_of_cm, top_nmi_maps_idx, top_ari_maps_idx, top_acc_maps_idx)
 run.save_test_metrics(list_of_nmi_test, list_of_ari_test, list_of_acc_test, list_of_cm_test, top_nmi_maps_idx_test, top_ari_maps_idx_test, top_acc_maps_idx_test)
        
-#plot training results
+
 run.plot_network_loss(list_of_losses, color = 'blue')
 run.plot_clustering_loss(list_of_losses, color = 'green')
 run.plot_total_loss(list_of_losses, color = 'brown')
@@ -166,7 +145,7 @@ run.plot_top_center_distances(list_of_center_distances, no_of_map, alpha=0.7,col
 run.plot_center_distances(list_of_center_distances, no_of_map, top_acc_maps_idx, alpha = 1.0, color = 'red')
 run.plot_best_maps_dual_bars(list_of_center_distances, list_of_acc, top_acc_maps_idx, alpha = 1.0, color_one = 'red', color_two = 'green')
 run.plot_nmi_ari(list_of_nmi, list_of_ari, top_acc_maps_idx)
-#plot testing results
+
 run.plot_acc_test(list_of_acc_test, top_acc_maps_idx_test, alpha = 0.8, color = 'red')
 run.plot_nmi_ari_test(list_of_nmi_test, list_of_ari_test,top_acc_maps_idx_test, alpha = 0.8)
 
